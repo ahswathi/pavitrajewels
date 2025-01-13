5385a0142cd06c801350c8af1248107b427f5adf
Swathi <swathiashok94@gmail.com>
2025-01-13 16:14:55 +0530

    testimonials prod cat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Website/pages/home/Home';
-import "./App.css";
 import Aboutus from './Website/pages/aboutus/Aboutus';
 import './App.css';
 import Product from './Website/pages/product/Product';
 import ProdDescription from './Website/pages/prodDescription/ProdDescription';
 import Navbar from './Website/components/maincomps/navbar/Navbar';
+import Home from './Website/pages/home/Home';
 
 const App = () => (
   <>
